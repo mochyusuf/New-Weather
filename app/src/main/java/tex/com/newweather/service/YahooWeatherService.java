5a954fb19e4e7a4735eb262b01285f016acaeadc
mochyusuf <mochyusuf100@gmail.com>
2018-03-23 15:39:48 +0700

    Part 4. Fix Bug

--- app/src/main/java/tex/com/newweather/service/YahooWeatherService.java
+++ app/src/main/java/tex/com/newweather/service/YahooWeatherService.java
@@ -38,7 +38,7 @@ public class YahooWeatherService {
             @Override
             protected String doInBackground(String... strings) {
                 String YQL = String.format("select * from weather.forecast where woeid in (select woeid from geo.places(1) where text=\"nome, ak\") and u ='c'", strings[0]);
-                String endpoint = String.format("https://query.yahooapis.com/v1/public/yql?q=select wind from weather.forecast where woeid in (select woeid from geo.places(1) where text='chicago, il')&format=json&callback=callbackFunction\"", Uri.encode(YQL));
+                String endpoint = String.format("https://query.yahooapis.com/v1/public/yql?q=%s&format=json", Uri.encode(YQL));
 
                 try {
                     URL url = new URL(endpoint);
